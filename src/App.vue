ddd32d75dc38aa33bd6d24d28a77e446e34efee6
Alexander <alexander.demidov.1997@gmail.com>
2024-10-24 13:18:50 +0300

    move api loader to app vue

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,33 @@
 import { RouterView } from 'vue-router'
 import './index.css'
 import HomeHeader from '@/components/HomeHeader.vue'
+import { onMounted, ref } from 'vue'
+import { loadEntities } from '@/state/entities'
+import type { EntityApi } from '@/api/entity'
+import ShowError from '@/components/ShowError.vue'
+
+interface Props {
+  apiLoader: EntityApi
+}
+const { apiLoader } = defineProps<Props>()
+
+const errorMessage = ref('')
+const errorHandler = (error: Error) => {
+  errorMessage.value = error.message
+  setTimeout(() => {
+    errorMessage.value = ''
+  }, 1000 * 5)
+}
+
+onMounted(() => {
+  loadEntities(apiLoader, errorHandler)
+})
 </script>
 
 <template>
   <HomeHeader/>
-  <main>
+  <main class="mx-2 md:mx-4">
     <RouterView />
+    <ShowError v-if="errorMessage" :error-message="errorMessage"/>
   </main>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,10 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-
 import AllEntities from '@/components/AllEntities.vue'
-import type { EntityApi } from '@/api/entity'
-import ShowError from '@/components/ShowError.vue'
-
-// Передаю с props класс для работы с api
-interface Props {
-  apiLoader: EntityApi
-}
-const { apiLoader } = defineProps<Props>()
-
-const allEntities = ref()
-const errorMessage = ref('')
-
-const loadEntities = async () => {
-  allEntities.value = await apiLoader
-    .getAll()
-    .catch(errorHandler)
-}
-
-const errorHandler = (error: Error) => {
-  errorMessage.value = error.message
-  setTimeout(() => {
-    errorMessage.value = ''
-  }, 1000 * 5)
-}
-
-onMounted(() => {
-  loadEntities()
-})
-
+import { allEntities } from '@/state/entities'
 </script>
 
 <template>
   <div class="w-full h-full flex items-center justify-center">
-    <AllEntities :entities="allEntities"/>
+    <AllEntities @deleteEntity="id => allEntities.delete(id)"/>
   </div>
-  <ShowError v-if="errorMessage" :error-message="errorMessage"/>
 </template>
