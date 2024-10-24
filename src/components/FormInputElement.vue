acb5aa6029663432e60e42bd4090ae556e0ae2af
Alexander <alexander.demidov.1997@gmail.com>
2024-10-25 01:20:13 +0300

    create ForminputElement, add validation for title

---
+++ src/components/FormInputElement.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+  label: string
+  errors?: string | undefined
+}
+
+const { label, errors } = defineProps<Props>()
+
+</script>
+
+<template>
+  <div class="w-full max-w-screen-xl min-w-[200px]">
+    <label class="block mb-2 text-sm text-slate-600">
+      {{ label }}
+    </label>
+    <slot name="input" />
+    <p v-if="errors" class="text-red-500 text-xs italic">{{ errors }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -5,6 +5,7 @@ import { EntitySchema } from '@/services/validation/entity'
 import { onMounted, onUpdated, ref } from 'vue'
 import { onBeforeRouteLeave, useRoute, useRouter } from 'vue-router'
 import { allEntities, maxKey } from '@/state/entities'
+import FormInputElement from '@/components/FormInputElement.vue'
 
 // for getting entity id and redirect to home_view after edit
 const route = useRoute()
@@ -45,11 +46,13 @@ onUpdated(() => {
   }
 })
 
-// create form, validate by zod schema, validateOnMont for highlighting required fields
+// create form, validate by zod schema
 const { defineField, errors, setFieldValue, setValues, handleSubmit, resetForm } = useForm({
   validationSchema: toTypedSchema(EntitySchema),
   validateOnMount: true,
   initialValues: {
+    title: '',
+    description: '',
     published: false,
     published_from: null,
   },
@@ -84,77 +87,90 @@ const handlePublishedFrom = (e: Event) => {
   }
   setFieldValue('published_from', value)
 }
-
-const inputClasses = 'w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200\n'
-  + '         rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400\n'
-  + '         hover:border-slate-300 shadow-sm focus:shadow'
 </script>
 
 <template>
   <div class="w-full mx-auto lg:max-w-screen-xl relative flex flex-col justify-center">
     <h2 class="text-center text-xl">{{ isEdit() ? 'Изменить' : 'Создать' }} запись</h2>
     <form class="flex flex-col gap-2 w-full" @submit.prevent="onSubmit">
-      <div v-if="entityId" class="w-full max-w-screen-xl min-w-[200px]">
-        <label class="block mb-2 text-sm text-slate-600">
-          Id
-        </label>
-        <input
-          :class="inputClasses"
-          :placeholder="entityId.toString()"
-          disabled
-        />
-      </div>
-      <div class="w-full max-w-screen-xl min-w-[200px]">
-        <label class="block mb-2 text-sm text-slate-600">
-          Title
-        </label>
-        <input
-          v-model="title"
-          v-bind="titleProps"
-          :class="inputClasses"
-          placeholder="Title ..."
-        />
-        <p class="text-red-500 text-xs italic">{{ errors.title }}</p>
-      </div>
-      <div class="w-full max-w-screen-xl min-w-[200px]">
-        <label class="block mb-2 text-sm text-slate-600">
-          Description
-        </label>
-        <textarea
-          v-model="description"
-          v-bind="descriptionProps"
-          :class="inputClasses"
-          rows="10"
-          placeholder="Description ..."/>
-        <p class="text-red-500 text-xs italic">{{ errors.description }}</p>
-      </div>
-      <div class="w-full max-w-screen-xl min-w-[200px]">
-        <label class="block mb-2 text-sm text-slate-600">
-          Published from
-        </label>
-        <input
-          type="checkbox"
-          v-model="published"
-          v-bind="publishedProps"
-          class=""
-        >
-        <p class="text-red-500 text-xs italic">{{ errors.published }}</p>
-      </div>
-      <div class="w-full max-w-screen-xl min-w-[200px]">
-        <label class="block mb-2 text-sm text-slate-600">
-          Published from
-        </label>
-        <input
-          :value="publishedFrom"
-          v-bind="publishedFromProps"
-          :class="inputClasses"
-          @input="handlePublishedFrom"
-          type="date"
-          placeholder="Published from ..."
-        />
-        <p class="text-red-500 text-xs italic">{{ errors.published_from }}</p>
-      </div>
+      <FormInputElement v-if="entityId" label="Id" >
+        <template #input>
+          <input
+            class="field animation"
+            :placeholder="entityId.toString()"
+            disabled
+          />
+        </template>
+      </FormInputElement>
+      <FormInputElement label="Title" :errors="errors.title" >
+        <template #input>
+          <input
+            v-model="title"
+            v-bind="titleProps"
+            class="field animation"
+            placeholder="Title ..."
+          />
+        </template>
+      </FormInputElement>
+      <FormInputElement label="Description" :errors="errors.description" >
+        <template #input>
+          <textarea
+            v-model="description"
+            v-bind="descriptionProps"
+            class="field animation"
+            rows="10"
+            placeholder="Description ..."/>
+        </template>
+      </FormInputElement>
+      <FormInputElement label="Published" :errors="errors.published">
+        <template #input>
+          <input
+            type="checkbox"
+            v-model="published"
+            v-bind="publishedProps"
+            class=""
+          >
+        </template>
+      </FormInputElement>
+      <FormInputElement label="Published from" :errors="errors.published_from" >
+        <template #input>
+          <input
+            :value="publishedFrom"
+            v-bind="publishedFromProps"
+            class="field animation"
+            @input="handlePublishedFrom"
+            type="date"
+            placeholder="Published from ..."
+          />
+        </template>
+      </FormInputElement>
       <button type="submit">Подтвердить изменения</button>
     </form>
   </div>
 </template>
+
+<style scoped>
+.field {
+  @apply
+  w-full
+  bg-transparent
+  placeholder:text-slate-400
+  text-slate-700
+  text-sm border
+  border-slate-200
+  rounded-md
+  px-3
+  py-2
+}
+.animation {
+  @apply
+  transition
+  duration-300
+  ease-in-out
+  focus:outline-none
+  focus:border-slate-400
+  hover:border-slate-300
+  shadow-sm
+  focus:shadow
+}
+</style>
