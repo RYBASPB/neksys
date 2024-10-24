fe3392b9625eee432827f24e5710164a6a4ceb9c
Alexander <alexander.demidov.1997@gmail.com>
2024-10-24 13:19:45 +0300

    load api loader on createApp

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { mockEntityApi } from '@/api/mockEntity'
 
-const app = createApp(App)
+const app = createApp(App, {
+  apiLoader: mockEntityApi,
+})
 
 app.use(router)
 
