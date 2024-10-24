e5fd8c4d1ca4b199a34e958ec730860c6cee1cef
Alexander <alexander.demidov.1997@gmail.com>
2024-10-25 01:20:28 +0300

    add entities loading element

---
+++ src/components/EntitiesList/EntitiesLoading.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="w-full">
+  <div class="text-center text-xl">
+    Загрузка
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
