8e7d6231d7aed07fc9e3db2005fb91ee8dcab82c
Alexander <alexander.demidov.1997@gmail.com>
2024-10-26 15:29:57 +0300

    clear form on update and leave route, watch for id update

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -2,19 +2,42 @@
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { EntitySchema } from '@/services/validation/entity'
-import { onMounted, onUpdated, ref } from 'vue'
-import { onBeforeRouteLeave, useRoute, useRouter } from 'vue-router'
+import { onMounted, ref, watch } from 'vue'
+import { onBeforeRouteLeave, onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router'
 import { allEntities, maxKey } from '@/state/entities'
 import FormInputElement from '@/components/FormInputElement.vue'
+import { NO_ENTITY_FOUND } from '@/shared/constants'
 
 // for getting entity id and redirect to home_view after edit
 const route = useRoute()
 const router = useRouter()
 const entityId = ref<number>()
 
-onBeforeRouteLeave(() => {
+const errorMessage = ref<string>('')
+
+const resetAll = () => {
   resetForm()
   entityId.value = NaN
+  errorMessage.value = ''
+}
+
+onBeforeRouteLeave(() => {
+  resetAll()
+})
+
+onBeforeRouteUpdate(() => {
+  resetAll()
+})
+
+watch(() => route.params.id, () => {
+  if (isEdit()) {
+    getEntityFromStateById()
+  }
+  if (isCreate()) {
+    console.log('Watch')
+    entityId.value = maxKey.value + 1
+    setFieldValue('id', entityId.value)
+  }
 })
 
 const isEdit = () => {
@@ -24,23 +47,21 @@ const isCreate = () => {
   return route.name === 'create'
 }
 
+function getEntityFromStateById() {
+  entityId.value = typeof route.params.id === 'string' ? parseInt(route.params.id) : NaN
+  const entity = allEntities.value.get(entityId.value)
+  if (entity) {
+    setValues(entity)
+    return
+  }
+  errorMessage.value = NO_ENTITY_FOUND
+}
+
 onMounted(() => {
   if (isEdit()) {
-    // TODO check id type
-    entityId.value = typeof route.params.id === 'string' ? parseInt(route.params.id) : NaN
-    const entity = allEntities.value.get(entityId.value)
-    if (entity) {
-      setValues(entity)
-    }
+    getEntityFromStateById()
   }
   if (isCreate()) {
-    entityId.value = maxKey.value + 1
-    setFieldValue('id', entityId.value)
-  }
-})
-
-onUpdated(() => {
-  if (isCreate() && !entityId.value) {
     entityId.value = maxKey.value + 1
     setFieldValue('id', entityId.value)
   }
@@ -58,7 +79,7 @@ const { defineField, errors, setFieldValue, setValues, handleSubmit, resetForm }
   },
 })
 
-const onSubmit = handleSubmit(async (values, actions) => {
+const onSubmit = handleSubmit(async (values) => {
   if (isEdit()) {
     // TODO await Api Edit
     allEntities.value.set(values.id, values)
@@ -69,8 +90,7 @@ const onSubmit = handleSubmit(async (values, actions) => {
     allEntities.value.set(entityId.value, values)
     maxKey.value = entityId.value
   }
-  entityId.value = NaN
-  actions.resetForm()
+  resetAll()
   await router.push('/')
 })
 
@@ -91,61 +111,64 @@ const handlePublishedFrom = (e: Event) => {
 
 <template>
   <div class="w-full mx-auto lg:max-w-screen-xl relative flex flex-col justify-center">
-    <h2 class="text-center text-xl">{{ isEdit() ? 'Изменить' : 'Создать' }} запись</h2>
-    <form class="flex flex-col gap-2 w-full" @submit.prevent="onSubmit">
-      <FormInputElement v-if="entityId" label="Id" >
-        <template #input>
-          <input
-            class="field animation"
-            :placeholder="entityId.toString()"
-            disabled
-          />
-        </template>
-      </FormInputElement>
-      <FormInputElement label="Title" :errors="errors.title" >
-        <template #input>
-          <input
-            v-model="title"
-            v-bind="titleProps"
-            class="field animation"
-            placeholder="Title ..."
-          />
-        </template>
-      </FormInputElement>
-      <FormInputElement label="Description" :errors="errors.description" >
-        <template #input>
+    <div v-if="errorMessage === NO_ENTITY_FOUND" class="text-center text-xl w-full">Такого элемента не существует</div>
+    <template v-else>
+      <h2 class="text-center text-xl">{{ isEdit() ? 'Изменить' : 'Создать' }} запись</h2>
+      <form class="flex flex-col gap-3 w-full items-center" @submit.prevent="onSubmit">
+        <FormInputElement v-if="entityId" label="Id">
+          <template #input>
+            <input
+              class="field animation"
+              :placeholder="entityId.toString()"
+              disabled
+            />
+          </template>
+        </FormInputElement>
+        <FormInputElement label="Title" :errors="errors.title">
+          <template #input>
+            <input
+              v-model="title"
+              v-bind="titleProps"
+              class="field animation"
+              placeholder="Title ..."
+            />
+          </template>
+        </FormInputElement>
+        <FormInputElement label="Description" :errors="errors.description">
+          <template #input>
           <textarea
             v-model="description"
             v-bind="descriptionProps"
             class="field animation"
             rows="10"
             placeholder="Description ..."/>
-        </template>
-      </FormInputElement>
-      <FormInputElement label="Published" :errors="errors.published">
-        <template #input>
-          <input
-            type="checkbox"
-            v-model="published"
-            v-bind="publishedProps"
-            class=""
-          >
-        </template>
-      </FormInputElement>
-      <FormInputElement label="Published from" :errors="errors.published_from" >
-        <template #input>
-          <input
-            :value="publishedFrom"
-            v-bind="publishedFromProps"
-            class="field animation"
-            @input="handlePublishedFrom"
-            type="date"
-            placeholder="Published from ..."
-          />
-        </template>
-      </FormInputElement>
-      <button type="submit">Подтвердить изменения</button>
-    </form>
+          </template>
+        </FormInputElement>
+        <FormInputElement label="Published" :errors="errors.published">
+          <template #input>
+            <input
+              type="checkbox"
+              v-model="published"
+              v-bind="publishedProps"
+              class=""
+            >
+          </template>
+        </FormInputElement>
+        <FormInputElement label="Published from" :errors="errors.published_from">
+          <template #input>
+            <input
+              :value="publishedFrom"
+              v-bind="publishedFromProps"
+              class="field animation"
+              @input="handlePublishedFrom"
+              type="date"
+              placeholder="Published from ..."
+            />
+          </template>
+        </FormInputElement>
+        <button class="w-fit py-3 px-4 bg-emerald-200 hover:bg-emerald-400 rounded-2xl" type="submit">Подтвердить изменения</button>
+      </form>
+    </template>
   </div>
 </template>
 
@@ -162,6 +185,7 @@ const handlePublishedFrom = (e: Event) => {
   px-3
   py-2
 }
+
 .animation {
   @apply
   transition
